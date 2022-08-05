79d528c2c82e01c716b62d07943130d334d199d1
Макс <max.bondarev2005@gmail.com>
2022-08-05 22:51:25 +0300

    #

--- fk_it.py
+++ fk_it.py
@@ -1,5 +1,4 @@
 """
-https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/python
-https://www.codewars.com/kata/555624b601231dc7a400017a/train/python
+
 """
 
---
+++ sum_of_pairs.py
@@ -0,0 +1,19 @@
+def sum_pairs(ints, s):
+    l = []
+    l_ind = []
+    for i in range(0, len(ints)):
+        for y in range(i+1, len(ints)):
+            if ints[i] + ints[y] == s:
+                a = ints[i]
+                b = ints[y]
+                l.append(a)
+                l.append(b)
+                l_ind.append(ints.index(ints[i]))
+                l_ind.append(ints.index(ints[y]))
+                print(l, l_ind)
+
+
+
+
+
+print(sum_pairs([10, 5, 2, 3, 7, 5],  10))
