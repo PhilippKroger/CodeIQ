4934ec02862488428f7dafb93cc16026442afecd
PhillKroger <max.bondarev2005@gmail.com>
2022-09-13 22:55:25 +0300

    #

--- The Most Important Implementations/hamming_code.py
+++ The Most Important Implementations/hamming_code.py
@@ -16,16 +16,33 @@ for i in range(1, len(b)):
         b[i] = int(a[0])
         a = a[1:]
 b = b[1:]
+print(b)
 
-
-
+rk = 2
 for i in range(k):
-
     ki = 2**i
     s = 0
+    j = i+1
+    while j <=m:
+        for j1 in range(i):
+            if j + j1 <= 12:
+                s += b[j + j1]
+            else:
+                break
+        j += rk
+    rk += 2
+    print(s)
+    if s % 2 == 1:
+        s = 1
+    else:
+        s = 0
+    b[2**i] = s
+print(b)
+
+"""
     for j in range(ki, m, 2*ki):
         print(j)
+
         for j1 in range(i):
             s += b[j+j1]
-
-print(b)
+        """
