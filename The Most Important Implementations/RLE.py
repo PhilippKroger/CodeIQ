4073a38ceab92754db73afbefe0b3f64cb4fd652
PhillKroger <max.bondarev2005@gmail.com>
2022-09-16 16:13:51 +0300

    #

---
+++ The Most Important Implementations/RLE.py
@@ -0,0 +1,21 @@
+def run_length_encoding(a):
+    """RLE"""
+    a += '*'
+    k = 0
+    x = ''
+    for i in range(len(a)-1):
+        if a[i] == a[i+1]:
+            k += 1
+        else:
+            x += str(k) + a[i]
+            k = 1
+
+    """Compression ratio"""
+    I = 0
+    for i in range(len(x)):
+        if not x[i].isdigit():
+            I += 1
+    K = len(x) / (I*2)
+    return 'RLE:', x, 'Compression ratio:', K
+
+print(run_length_encoding('ABBCCCDDDD'))
