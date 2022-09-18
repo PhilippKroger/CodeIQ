1ba92e65dea212a526305be2a047c5a37c7f09bf
PhillKroger <max.bondarev2005@gmail.com>
2022-09-18 15:37:33 +0300

    #

--- The Most Important Implementations/RLE.py
+++ The Most Important Implementations/RLE.py
@@ -20,3 +20,28 @@ def run_length_encoding(a):
 
 
 print(run_length_encoding('ABBCCCDDDD'))
+
+
+def de_rle(a):
+
+    l = []
+
+    for i in range(0, len(a), 8):
+        l.append(a[i:i+8])
+
+    x = ''
+    for i in range(len(l)):
+        ln = int(l[i][1:], 2)
+
+        if l[i][0] == '1':
+            x += str(ln)
+            x += ln * chr(int(l[i+1], 2))
+            continue
+        elif l[i][0] == '0':
+            x += str(ln)
+            for j in range(ln):
+                x += chr(int(l[i+1+j], 2))
+    return x
+
+print(de_rle('1000111111000000000000101100000111000010'))
+
